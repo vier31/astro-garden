af2dc572fbddd5f453f53271881581665c949a40
Jan Schröder <jan.schroeder@statista.com>
2022-10-17 04:17:22 +0200

    added service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import vue from "@astrojs/vue";
+import serviceWorker from "astrojs-service-worker";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), vue()]
+  integrations: [serviceWorker(), mdx(), vue()]
 });
